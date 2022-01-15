51030a00e70e812dc73e8aa025d42416acfe01c6
Mateus Costa <leitemcs@hotmail.com>
2022-01-15 17:33:45 -0300

    att4 nv dificuldade

--- scriptfacil.js
+++ scriptfacil.js
@@ -10,7 +10,7 @@ var div = document.querySelector('#div1')
 var respostas = document.querySelector('#respostas')
 var total = 0
 var acertos = 0
-var t = 1
+var t = 30
 
 document.addEventListener('keydown', function (e) {
     e.key == 'Escape' ? window.location.href = 'indexfacil.html' : {}
