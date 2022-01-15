91e81b3766b7389b2b9da6f68dc338c9d6d862be
Mateus Costa <leitemcs@hotmail.com>
2022-01-15 16:08:19 -0300

    att 15/01

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
 var btn = document.querySelector('#btn')
 btn.addEventListener('click', clicar)
+
+function clicar() {
+    window.location.href = 'index2.html'
+}
+
+document.addEventListener('keydown', function (e) {
+    e.key === 'Enter' ? btn.click() : {}
+})
